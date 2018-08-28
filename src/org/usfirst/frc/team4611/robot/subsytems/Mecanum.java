7547a772fd8f4b7a56b4e65fe964ebf81eb39ee3
Connor2573 <connor.park2573@gmail.com>
2018-08-28 12:10:22 -0400

    adding an untracked mecanum file

---
+++ src/org/usfirst/frc/team4611/robot/subsytems/Mecanum.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4611.robot.subsytems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class Mecanum extends Subsystem implements iMecanum{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
